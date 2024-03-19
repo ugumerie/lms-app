30f2b09e6188a165377d9cbebd0aa070e78de5e5
Ugochukwu Umerie <umerieugochukwu@gmail.com>
2024-03-19 13:25:01 +0100

    feat: Implement chapter publish actions

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -76,7 +76,7 @@ export const ChaptersList = ({
                     className={cn(
                       "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
                       chapter.isPublished &&
-                        "bg-sky-700 border-sky-200 text-sky-700"
+                        "bg-sky-200 border-sky-200 text-sky-700"
                     )}
                     ref={provided.innerRef}
                     {...provided.draggableProps}
@@ -85,7 +85,7 @@ export const ChaptersList = ({
                       className={cn(
                         "px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
                         chapter.isPublished &&
-                          "border-r-sky-200 hover:bg-sky-200"
+                          "border-r-sky-200 border-r-sky hover:bg-sky-200"
                       )}
                       {...provided.dragHandleProps}
                     >
