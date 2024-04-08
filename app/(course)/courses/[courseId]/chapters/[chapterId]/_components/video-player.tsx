3f92f92bffc4a74e4050c2db529eceed6a99418b
Ugochukwu Umerie <umerieugochukwu@gmail.com>
2024-04-08 12:57:01 +0100

    feat: Implement chapter progress functionality

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -30,6 +30,31 @@ export const VideoPlayer = ({
   title,
 }: VideoPlayerProps) => {
   const [isReady, setIsReady] = useState(false);
+  const router = useRouter();
+  const confetti = useConfettiStore();
+
+  const onEnd = async () => {
+    try {
+      if (completeOnEnd) {
+        await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
+          isCompleted: true
+        })
+      }
+
+      if (!nextChapterId) {
+        confetti.onOpen()
+      }
+
+      if (nextChapterId) {
+        router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+      }
+
+      toast.success("Progress updated")
+      router.refresh()
+    } catch {
+      toast.error("Something went wrong")
+    }
+  }
 
   return (
     <div className="relative aspect-video">
@@ -49,7 +74,7 @@ export const VideoPlayer = ({
           title={title}
           className={cn(!isReady && "hidden")}
           onCanPlay={() => setIsReady(true)}
-          onEnded={() => {}}
+          onEnded={onEnd}
           autoPlay
           playbackId={playbackId}
         />
